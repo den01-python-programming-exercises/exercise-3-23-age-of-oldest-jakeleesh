25febf67c38b0934b06eb0f300a5504477d7e100
Jake Lee <jake.lee@fdmgroup.com>
2021-10-09 15:59:22 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    greatest = 0
+    while True:
+        details = input()
+        if (details != ""):
+            details_split = details.split(",")
+            if (int(details_split[1]) > greatest):
+                greatest = int(details_split[1])
+        else:
+            break
+    print("Age of the oldest: " + str(greatest))
 
 if __name__ == '__main__':
     main()
